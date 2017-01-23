8be5065babaedb081aadf3102a958ea476ff6999
SylarChen <chenheng415@163.com>
2017-01-23 14:13:08 +0800

    Update installShadowSocksClient.sh

--- ShadowSocks/installShadowSocksClient.sh
+++ ShadowSocks/installShadowSocksClient.sh
@@ -4,7 +4,7 @@ curl https://bootstrap.pypa.io/get-pip.py | python
 # install cliebt
 pip install shadowsocks
 
-sslocal -s 104.194.69.242 -p 2223 -b 127.0.0.1 -l 1080 -k sylarchen -t 600 -m aes-256-cfb
+sslocal -s ****** -p 2223 -b 127.0.0.1 -l 1080 -k ****** -t 600 -m aes-256-cfb
 
 sslocal -c config.json -v
 
